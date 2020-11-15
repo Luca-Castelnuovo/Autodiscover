519919785333134e0615dca54f6b5a9dc77b4a79
Luca Castelnuovo <luca@castelnuovo.xyz>
2020-11-15 14:22:53 +0100

    update url's

--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,6 @@ use CQ\Routing\Route;
 Route::$router = $router->get();
 Middleware::$router = $router->get();
 
-Route::any('/mail/config-v1', 'AutoMXController@index');
 Route::any('/mail/config-v1.1.xml', 'AutoMXController@index');
+Route::any('/autodiscover/autodiscover.xml', 'AutoMXController@index');
+Route::any('/Autodiscover/Autodiscover.xml', 'AutoMXController@index');
